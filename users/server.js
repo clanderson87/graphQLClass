9ea70ef955ca7bebdc69653231c887a646aa734c
Chris Anderson <cl.anderson8@gmail.com>
2017-03-14 14:15:06 -0500

    integrating GraphQL basics

--- users/server.js
+++ users/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true //only intended for dev servers
+}));
 
 app.listen(4000, () => {
   console.log("listening");

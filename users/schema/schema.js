606f65164ea099aee9cfbd8c83cbe415af66e55d
Chris Anderson <cl.anderson8@gmail.com>
2017-03-14 14:35:57 -0500

    graphiql is on screen

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,9 +1,20 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   GraphQLObjectType,
   GraphQLInt,
-  GraphQLString
+  GraphQLString,
+  GraphQLSchema
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bob', age: 23 },
+  { id: '24', firstName: 'Bill', age: 24 },
+  { id: '25', firstName: 'Buck', age: 25 },
+  { id: '26', firstName: 'Biff', age: 26 },
+  { id: '27', firstName: 'Benny', age: 73 },
+  { id: '28', firstName: 'Berry', age: 83 }
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -21,9 +32,13 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-
+        return _.find(users, {id: args.id});
       } //resolve's job is to go out and grab REAL data. parentValue will almost never be used. More info later.
         //args represents the args object above. the args argument in this case will be expected to have an id prop.
     }
   }
 });
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})

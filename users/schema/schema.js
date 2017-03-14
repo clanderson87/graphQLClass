43d2c24943a69f2cca4cdd1e3af93440f76ce14f
Chris Anderson <cl.anderson8@gmail.com>
2017-03-14 14:29:33 -0500

    created RootQuery

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,8 +8,22 @@ const {
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    id: {type: GraphQLString },
-    firstName: {type: GraphQLString}, //triple check the captialization on these type declarations
-    age: {type: GraphQLInt}
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString }, //triple check the captialization on these type declarations
+    age: { type: GraphQLInt }
   }
-}); // this is not the schema itself, but this is the GraphQL schema blueprint.
+}); // this is not the schema itself, but the GraphQL schema blueprint.
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: { // this reads, IF looking for a user, PROVIDED an id, WILL RETURN a UserType
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      } //resolve's job is to go out and grab REAL data. parentValue will almost never be used. More info later.
+        //args represents the args object above. the args argument in this case will be expected to have an id prop.
+    }
+  }
+});
